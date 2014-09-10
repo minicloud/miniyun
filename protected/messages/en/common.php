9cbcae76e3312722f8c8f64540295879d576e6ba
jim <jim@miniyun.cn>
2014-09-10 17:51:23 +0800

    把迷你云作为插件的一部分处理

--- protected/messages/en/common.php
+++ protected/messages/en/common.php
@@ -5,4 +5,5 @@
 return  array(
     //插件安装
     "plugin_has_install"=>"Enabled {plugin_name} plug-in, plug-in system of the same type can only keep a. You can set the {plugin_name} disabled, enabled the new plug-in.",
+    "plugin_miniyun_description"=>"{app_name} main installation package",
 );
--- protected/messages/zh_tw/common.php
+++ protected/messages/zh_tw/common.php
@@ -5,4 +5,5 @@
 return  array(
     //插件安装
     "plugin_has_install"=>"已啟用插件{plugin_name}，同類型的插件系統只能保留壹個。您可將{plugin_name}停用後，啟用新插件。",
+    "plugin_miniyun_description"=>"{app_name}主安装包",
 );
