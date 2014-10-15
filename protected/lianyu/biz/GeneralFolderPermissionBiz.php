6de309fe7ad3b8e15dc5060e3d26f392bc466682
fingergly <390810691@qq.com>
2014-10-15 14:16:33 +0800

    获取当前目录的父目录或子目录被共享

--- protected/lianyu/biz/GeneralFolderPermissionBiz.php
+++ protected/lianyu/biz/GeneralFolderPermissionBiz.php
@@ -6,6 +6,12 @@
  * Time: 上午9:56
  */
 class GeneralFolderPermissionBiz extends  MiniBiz{
+    public function __construct($path){
+         if($this->isChildrenShared($path)||$this->isParentShared($path)){
+             return true;
+         }
+        return false;
+    }
     /**
      * 判断文件夹内是否有子文件被共享
      * @param $path
@@ -16,6 +22,21 @@ class GeneralFolderPermissionBiz extends  MiniBiz{
         $userPermissions = MiniUserPrivilege::getInstance()->searchFilePath($path);
         if(count($groupPermissions)>0||count($userPermissions)>0){
             return true;
+        }
+        return false;
+    }
+    /**
+     * 判断文件夹内是否有父目录被共享
+     */
+    public function isParentShared($path){
+        $arr = explode('/',$path);
+        $parentPath = "/".$arr[1];
+        for($i=2;$i<count($arr);$i++){
+            $parentPath = $parentPath."/".$arr[$i];
+            $file = MiniFile::getInstance()->getByFilePath($parentPath);
+            if($file['file_type']==2){
+                return true;
+            }
         }
         return false;
     }
