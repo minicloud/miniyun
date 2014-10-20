8fad48a3f7232b474918280c23f7a74fd566b32b
fingergly <390810691@qq.com>
2014-10-20 16:14:28 +0800

    文件下载提交

--- protected/lianyu/share/MiniShare.php
+++ protected/lianyu/share/MiniShare.php
@@ -29,15 +29,14 @@ class MiniShare
      * @param $relativePath
      * @return Array|NULL
      */
-    public function getMinFileMetaByPath($relativePath){
+    public function getMinFileMetaByPath($absolutePath){
         $data = array();
-        //如果是根目錄
-        $absolutePath = MiniUtil::getAbsolutePath($this->user["id"], $relativePath);
-        if(empty($relativePath)){
+        if(empty($absolutePath)){
+            //如果是根目錄
+            $absolutePath = MiniUtil::getAbsolutePath($this->user["id"], $absolutePath);
             $data["ori_path"] = $absolutePath;
             return $data;
         }
-        $absolutePath = MiniUtil::getAbsolutePath($this->user["id"], $relativePath);
         $file = MiniFile::getInstance()->getByPath($absolutePath);
         if(!empty($file)){
             $data["mime_type"] = $file["mime_type"];
@@ -49,23 +48,23 @@ class MiniShare
         if(count($info)<2){
             return NULL;
         }
-        //共享目录都在根目录下，在这里拼接出file_mates需要的file_path
-        $shareFilePath = MiniUtil::joinPath($info[1],$info[2]);
-        //查询所在的根目录是否是共享目录
-        $meta = MiniFileMeta::getInstance()->getFileMeta($shareFilePath,"shared_folders");
-        if($meta===NULL){
-            return NULL;
-        }
-        $metaValue = $meta["meta_value"];
-        //获得发起人所在的目录根目录信息，并且拼接出文件原始路径
-        $shareDetail = unserialize($metaValue);
-        $initiatorRootPath = $shareDetail["path"];
-        $shareRelativePath = substr($absolutePath,strlen($shareFilePath));
-        $oriPath = $initiatorRootPath.$shareRelativePath;
-        $file = MiniFile::getInstance()->getByPath($oriPath);
-        $data["mime_type"] = $file["mime_type"];
-        $data["version_id"] = $file["version_id"];
-        $data["ori_path"] = $file["file_path"];
-        return $data;
+//        //共享目录都在根目录下，在这里拼接出file_mates需要的file_path
+//        $shareFilePath = MiniUtil::joinPath($info[1],$info[2]);
+//        //查询所在的根目录是否是共享目录
+//        $meta = MiniFileMeta::getInstance()->getFileMeta($shareFilePath,"shared_folders");
+//        if($meta===NULL){
+//            return NULL;
+//        }
+//        $metaValue = $meta["meta_value"];
+//        //获得发起人所在的目录根目录信息，并且拼接出文件原始路径
+//        $shareDetail = unserialize($metaValue);
+//        $initiatorRootPath = $shareDetail["path"];
+//        $shareRelativePath = substr($absolutePath,strlen($shareFilePath));
+//        $oriPath = $initiatorRootPath.$shareRelativePath;
+//        $file = MiniFile::getInstance()->getByPath($oriPath);
+//        $data["mime_type"] = $file["mime_type"];
+//        $data["version_id"] = $file["version_id"];
+//        $data["ori_path"] = $file["file_path"];
+//        return $data;
     }
 }
