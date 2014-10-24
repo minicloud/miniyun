adc7ff8a980ee4d9f73031c61fc930f2c72fd6d8
fingergly <390810691@qq.com>
2014-10-24 17:30:33 +0800

    edit

--- protected/lianyu/biz/AlbumBiz.php
+++ protected/lianyu/biz/AlbumBiz.php
@@ -37,22 +37,64 @@ class AlbumBiz  extends MiniBiz{
         $timeLine['total']=count($albumList);
         return $timeLine;
     }
-
+    public function getAllSharedPath($userId){
+        $publicFiles = MiniFile::getInstance()->getPublics();
+        $groupShareFiles  = MiniGroupPrivilege::getInstance()->getAllGroups();
+        $userShareFiles   = MiniUserPrivilege::getInstance()->getAllUserPrivilege();
+        $filePaths  = array();
+        $shareFiles = array_merge($publicFiles,$groupShareFiles,$userShareFiles);
+        foreach($shareFiles as $file){
+            $file = MiniFile::getInstance()->getByPath($file['file_path']);
+            if(!empty($file)){
+                if((($file['parent_file_id'] == 0) && $file['is_deleted'] == 0) || (($file['file_type'] == 2)&&($file['user_id'] != $userId))){
+                    $filePaths[] = $file['file_path'];
+                }
+            }
+        }
+        $filePaths = array_unique($filePaths);
+        return $filePaths;
+    }
     public function getPage($page,$pageSize){
         $list=array();
         $pageSet=($page-1)*$pageSize;
         $user      = $this->user;
-        $albumList = MiniFile::getInstance()->getFileListPage( $pageSet,$pageSize,$user["id"],"image");
-        foreach($albumList as $value){
+        $filePaths = $this->getAllSharedPath($user['id']);
+        $albumList = MiniFile::getInstance()->getFileByUserType($user["id"],"image");
+        $fileList["total"]=count($albumList);
+        //获取当前文件夹下的子文件
+        foreach($filePaths as $filePath){
+            $images = MiniFile::getInstance()->searchFileByPathType($filePath);
+            foreach($images as $image){
+                $imageArr[] = $image;
+            }
+        }
+        $sharedImgTotal = count($imageArr);
+        if( $pageSet>$sharedImgTotal){
+            $pageSet = $pageSet-$sharedImgTotal;
+            $albums = MiniFile::getInstance()->getFileListPage( $pageSet,$pageSize,$user["id"],"image");
+        }else{
+            if($page*$pageSize<=$sharedImgTotal){
+                 for($index=$pageSet;$index<$page*$pageSize-1;$index++){
+                     $albums[] = $imageArr[$index];
+                 }
+            }else{
+                for($index=$pageSet;$index<$sharedImgTotal;$index++){
+                    $albumShared[] = $imageArr[$index];
+                }
+                $albumList =   $albums = MiniFile::getInstance()->getFileListPage(0,$pageSize*$page-$sharedImgTotal,$user["id"],"image");
+                $albums = array_merge($albumList,$albumShared);
+            }
+        }
+
+        foreach($albums as $value){
             $data["filename"]=$value['file_name'];
             $data["fileSize"]=$value['file_size'];
             $data["path"]= MiniUtil::getRelativePath($value['file_path']);
             $data["createTime"]=$value['file_create_time'];
             $list[]=$data;
         }
-        $albumList = MiniFile::getInstance()->getFileByUserType($user["id"],"image");
         $fileList['list']=$list;
-        $fileList["total"]=count($albumList);
+
         return $fileList;
     }
 
