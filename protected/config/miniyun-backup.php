6e734420c822cb04f9df70e1974daa47e19fba0c
jim <jim@miniyun.cn>
2015-01-19 09:19:06 +0800

    minidoc

---
+++ protected/config/miniyun-backup.php
@@ -0,0 +1,11 @@
+<?php
+/** 
+* 迷你云后台服务配置文件
+* 包括：迷你文档/迷你存储/迷你搜索
+*/
+/** 迷你云地址，可以让迷你文档访问的地址 */
+define('MINIYUN_HOST', 'http://t.miniyun.cn');
+/** 迷你搜索SPHINX地址 */
+define('SPHINX_HOST', '127.0.0.1:9932');
+/** 迷你文档服务器地址 */
+define('MINIDOC_HOST', 'http://minidoc.miniyun.cn');
