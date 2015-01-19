1cb8946d21867a4f8cf5b16672bfa08c731f64a2
ryan <276489620@qq.com>
2015-01-19 09:51:20 +0800

    post

--- protected/commands/DocConvertCommand.php
+++ protected/commands/DocConvertCommand.php
@@ -48,18 +48,10 @@ class DocConvertCommand extends CConsoleCommand
      */
     private function post($url,$params){
         $data = array ('task' =>json_encode($params));
-        $data = http_build_query($data);
-        $opts = array (
-            'http' => array (
-                'method' => 'POST',
-                'header'=> "Content-type: application/x-www-form-urlencodedrn" .
-                    "Content-Length: " . strlen($data) . "rn",
-                'content' => $data
-            )
-        );
-        $context = stream_context_create($opts);
-        $result = file_get_contents($url, false, $context);
-        return $result;
+        $http = new HttpClient();
+        $http->post($url,$data);
+
+        return $http->get_body();
     }
     /**
      * 定时任务入口
