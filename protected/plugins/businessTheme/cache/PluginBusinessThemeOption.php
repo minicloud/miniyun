122c0abcdf0643677739746420cbc4b32e7ebd64
jim <jim@miniyun.cn>
2015-02-10 15:28:56 +0800

    1.8.0

--- protected/plugins/businessTheme/cache/PluginBusinessThemeOption.php
+++ protected/plugins/businessTheme/cache/PluginBusinessThemeOption.php
@@ -9,7 +9,7 @@ class PluginBusinessThemeOption extends MiniVersion{
             'companyName' => '成都迷你云科技有限公司',
             'companyEnglishName' => 'Chengdu Mini cloud Technology Co. Ltd.',
             'productName' => '迷你云',
-            'logo'     => $host.'static/images/plugins/pluginTheme/logo.gif',
+            'logo'     => $host.'static/images/logo.png',
             'carouselImagesUrl' => array($host.'static/images/plugins/pluginTheme/default.png'),
             'helpName' => '帮助',
             'helpUrl'  => 'http://bbs.miniyun.cn'
