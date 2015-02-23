8cf7065f54e41a7ef0817ede787f5fb6bb07244b
jim <jim@miniyun.cn>
2015-02-23 16:50:15 +0800

    支持生成冗余备份记录

---
+++ protected/plugins/miniStore/models/ReplicateTask.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 冗余备份任务表
+ * @author app <app@miniyun.cn>
+ * @link http://www.miniyun.cn
+ * @copyright 2015 Chengdu MiniYun Technology Co. Ltd.
+ * @license http://www.miniyun.cn/license.html
+ * @since 1.8
+ */
+class ReplicateTask extends CMiniyunModel
+{
+
+    /**
+     * Returns the static model of the specified AR class.
+     * return User the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return Yii::app()->params['tablePrefix'].'replicate_tasks';
+    }
+
+}
