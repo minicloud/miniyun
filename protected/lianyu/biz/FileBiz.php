8137564c003d5c99c4b7f9219e37fb34dcbdb7ae
ryan <app@miniyun.cn>
2015-06-09 04:07:22 -0400

    删除预览权限测试代码

--- protected/lianyu/biz/FileBiz.php
+++ protected/lianyu/biz/FileBiz.php
@@ -314,11 +314,11 @@ class FileBiz  extends MiniBiz{
      */
     public function txtContent($path,$signature){
         $share = new MiniShare();
-        // $fileBiz = new FileBiz();
-        // $canRead = $fileBiz->privilege($path);
-        // if(!$canRead){
-        //     throw new MFileopsException( Yii::t('api','no permission'),MConst::HTTP_CODE_409);
-        // }
+        $fileBiz = new FileBiz();
+        $canRead = $fileBiz->privilege($path);
+        if(!$canRead){
+            throw new MFileopsException( Yii::t('api','no permission'),MConst::HTTP_CODE_409);
+        }
         $minFileMeta = $share->getMinFileMetaByPath($path);
         $file = array();
         $content = MiniFile::getInstance()->getTxtContent($minFileMeta['ori_path'],$signature);
@@ -419,8 +419,6 @@ class FileBiz  extends MiniBiz{
             }else{
                 return false;
             }
-        }else{
-            return true;
         }
         //用户已登陆情况下，判断文件是否属于自己
         $pathArr = explode('/',$path);
