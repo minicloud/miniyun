d849371603f82921709cfb06694ff211c247e23a
app@miniyun.cn <app@miniyun.cn>
2015-06-12 15:04:02 +0800

    revert code

--- protected/plugins/miniStore/MiniStoreModule.php
+++ protected/plugins/miniStore/MiniStoreModule.php
@@ -107,7 +107,7 @@ class MiniStoreModule extends MiniPluginModule {
         //回调地址
         $callbackUrl = MiniHttp::getMiniHost()."api.php?node_id=".base64_encode($node["id"]);
         foreach ($params as $key => $value) {
-            $callbackUrl .="&".$key."=".base64_encode($value);
+            $callbackUrl .="&".$key."=".base64_encode(urlencode($value));
         }
         $callbackUrl .="&encode=base64";
         $siteId   = MiniSiteUtils::getSiteID();
