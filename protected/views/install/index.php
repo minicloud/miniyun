d5206536a0c7f11f8983159341773baf6039ee69
jim <jim@miniyun.cn>
2016-09-12 15:46:27 +0800

    add app m3-login

--- protected/views/install/index.php
+++
@@ -1,9 +0,0 @@
-<?php $name = 'en' == Yii::app()->getLanguage() ? NAME_EN: NAME_ZH;?>
-<div class='main'>
-<p><?php echo Yii::t("front_common", "install_index_title", array("{app_name}"=>$name))?></p>
-<p><?php echo Yii::t("front_common", "install_index_prompt_1")?></p>
-<p><?php echo Yii::t("front_common", "install_index_prompt_2")?></p>
-<br>
-<a style="width:150px;margin-left:150px;cursor:pointer;" href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/install/setup1" class="btn_white" ><span class="b1"><span class="b2"><?php echo Yii::t("front_common", "install_index_button")?></span></span></a>
-<br>
-</div>
--- protected/views/install/setup3.php
+++
@@ -1,8 +0,0 @@
-<?php $name = 'en' == Yii::app()->getLanguage() ? NAME_EN: NAME_ZH;?>
-<br>
-<div class="main">
-<p>账户的默认密码为:&nbsp;admin</p>
-<p><?php echo Yii::t("front_common", "install_setup4_title", array("{app_name}"=>$name))?></p>
-    <a style="width:120px;margin-left:150px;cursor:pointer;" href="<?php echo Yii::app()->createUrl("site/login");?>" onclick="" class="btn_white" ><span class="b1"><span class="b2"><?php echo Yii::t("front_common", "install_setup4_button")?></span></span></a>
-<br>
-</div>
